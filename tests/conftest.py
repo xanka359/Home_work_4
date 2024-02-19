3947f8aa87f01404c5901c27855a418066472474
Oksana Bondareva <xanka359@yandex.ru>
2024-02-20 01:00:58 +0300

    the second one

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.fixture(scope='function', autouse=True)
 def manage_browser():
-    browser.config.base_url = 'https://www.google.com/'
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.timeout = 6.0
 
     yield
